96b36d04b7dbf94d7845270b6026c60f516aacd9
Ebrar-S <108938155+Ebrar-S@users.noreply.github.com>
2024-12-16 16:26:23 +0300

    Initial commit

---
+++ lib/auth_service.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signInAnonymously() async {
+    try {
+      final UserCredential userCredential = await _auth.signInAnonymously();
+      return userCredential.user;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+  User? getCurrentUser() {
+    return _auth.currentUser;
+  }
+}
---
+++ lib/firebase_options.dart
@@ -0,0 +1,17 @@
+// Generated file. Do not edit.
+//
+
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: 'AIzaSyDzNNoX9chAQ4Wmzc7GMuLnfF1wLM3UHgs',
+      appId: '1:784639245690:android:bc94aa0a5e594adbf5eac3',
+      messagingSenderId: '784639245690',
+      projectId: 'untitled-be5e3',
+    );
+  }
+}
